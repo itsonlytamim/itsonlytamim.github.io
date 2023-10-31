aeb792e0b641c948d96ece8935c5b9625209ece9
Md. Shamiul Islam Tamim <56086036+itsonlytamim@users.noreply.github.com>
2023-10-31 13:22:26 +0600

    Update data.js

--- data.js
+++ data.js
@@ -71,162 +71,46 @@ export const skills = [
 export const projects = {
     webProjects: [
         {
-            projectName: "Programming Diaries",
+            projectName: "Moveez (A movie ticket management System)",
             image: "images/programmingdiaries.png",
             summary:
-                "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+                "Moveez is a movie ticket management system that enables users to book tickets for movies available in nearby cinema halls.",
             preview: "https://programmingdiaries.herokuapp.com/",
-            techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
+            techStack: ["C#","ASP.NET Web API","HTML","CSS","Angular Js","Bootstrap","Microsoft SQL Server","git"],
         },
         {
-            projectName: "Find Your Bank",
+            projectName: "StudentDoc App",
             image: "images/findyourbank.png",
             summary:
-                "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+                "StudentDoc App is a document management platform that empowers students to efficiently manage their documents, allowing them to upload, update, delete, and download files for future use.",
             preview: "https://clever-fermi-0d5d76.netlify.app",
-            techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+            techStack: ["C#","ASP.NET Web API","HTML","CSS","Angular Js","Bootstrap","Microsoft SQL Server","git"],
         },
+       
         {
-            projectName: "Web Portfolio",
-            image: "images/portfolio.png",
-            summary:
-                "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-            preview: "https://github.com/vinaysomawat/Travographer-Portal",
-            techStack: ["HTML", "Bootstrap", "JavaScript"],
-        },
-        {
-            projectName: "Resume Builder",
-            image: "images/resume-builder.png",
-            summary:
-                "Browser based editor to build and download Resumes in a customizable templates.",
-            preview: "https://vinaysomawat.github.io/Resume-Builder",
-            techStack: ["HTML", "Bootstrap", "JavaScript"],
-        },
     ],
     softwareProjects: [
         {
-            projectName: "Pizza Ordering ChatBot",
+            projectName: "Chaya Healthcare Solution",
             image: "images/pizzaorderchatbot.png",
             summary:
-                "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+                "Chaya Healthcare Solution is a chat application that facilitates communication between patients and doctors, designed to improve healthcare interactions.",
             preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
-            techStack: ["Dailogflow", "Firebase"],
-        },
-        {
-            projectName: "WhatsApp-Bot",
-            image: "images/whatsappbot.jpg",
-            summary:
-                "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-            preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
-            techStack: ["Selenium", "Chrome Webdriver", "Python"],
-        },
-        {
-            projectName: "Bill Generator",
-            image: "images/billgenerator.png",
-            summary:
-                "GUI to transfer data to excel sheets and generate bills on the local shops.",
-            preview: "https://github.com/vinaysomawat/Bill-Generator",
-            techStack: ["Tkinter", "Openxlpy", "Python"],
+            techStack: ["C#",".NET Framework","Windows Forms (WPF)","Microsoft SQL Server"],
         },
     ],
     androidProjects: [
         {
-            projectName: "NITW-CSE",
+            projectName: "Classification-based Machine Learning Model Development For Diabetes Prediction",
             image: "images/nitwcse.jpg",
             summary:
-                "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+                "Classification-based Machine Learning Model Development For Diabetes Prediction is a data-driven project aimed at predicting diabetes with the power of machine learning and data science technique",
             preview: "https://github.com/vinaysomawat/NITW-CSE",
-            techStack: ["JAVA", "XML", "Android"],
-        },
-        {
-            projectName: "CareerHigh-App",
-            image: "images/carrerhigh.png",
-            summary:
-                "The Application display the webpages of website careerhigh.in in android devices.",
-            preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-            techStack: ["JAVA", "XML", "Android"],
-        },
-    ],
-    freelanceProjects: [
-        {
-            projectName: "SnylloAir.com",
-            image: "images/snylloair.png",
-            summary:
-                "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-            preview: "https://www.snylloair.com/",
-            techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-        },
-        {
-            projectName: "Delivery+",
-            image: "images/AM-Logo-.png",
-            summary: "Android Application to display website in android devices.",
-            preview:
-                "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-            techStack: ["Android", "JAVA", "Play Store"],
+            techStack: ["Machine Learning Algorithms","Python","Scikit-learn","Pandas","Jupyter Notebooks","Diabetes Datasets"],
         },
     ],
 };
 
-export const experience = [
-    {
-        title: "Shiprocket (Bigfoot Solution Private Limited)",
-        duration: "September 2022 - Present",
-        subtitle: "Software Engineer",
-        details: [
-            "Working in support and escalation team."
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-        icon: "truck ",
-    },
-    {
-        title: "Biofourmis India Private Limited",
-        duration: "April 2022 - Jul 2023",
-        subtitle: "Software Engineer",
-        details: [
-            `Implemented microfrontends using the Module Federation Plugin in Angular.
-            Rendered components from remote modules/repositories, enabling a modular and scalable architecture`,
-            `Upgraded application with a dynamic base URL for flexible operation across multiple regions and verticals, leading to
-            recognition with the Biofourmis Bravo award in Q1, 2023.`,
-            `Implemented RxState for managing local state in components, resulting in elegant and reactive facades.`,
-            `Utilized Twilio-Video for group video call rooms and Countly for Web Analytics.`,
-            `Integrated NGXS WebSocket, enhancing state management and replacing legacy service injections.`,
-            `Implemented extensive RxJS usage for reactive programming, resulting in efficient data handling, improved state
-            management, and enhanced application performance.`,
-            `Dramatically optimized load time and performance through code splitting, lazy loading, caching, and preload`,
-            `Enhanced video call experience by implementing real-time switching of I/O devices and audio level indicators,
-            resulting in reduced audio issues and increased user satisfaction.`,
-            `Owned and led successful Angular application upgrades from version 12 to 13 and 14, improving performance
-            and enhancing features.`,
-        ],
-        tags: ["JavaScript", "Angular", "RxJS", "NGXS", "TypeScript", "RxState", "Webpack", "Optimization"],
-        icon: "heartbeat",
-    },
-    {
-        title: "Novopay Solutions Private Limited",
-        duration: "June 2020 - April 2022",
-        subtitle: "Software Engineer",
-        details: [
-            `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
-            and Money transfer.`,
-            `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
-            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
-            `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
-            `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-        icon: "qrcode",
-    },
-    {
-        title: "ThinkPedia LLP",
-        duration: "May 2019 - June 2019",
-        subtitle: "SDE Intern",
-        details: [
-            `Developed a customer web application for social media management, supporting the advertisement domain.`,
-        ],
-        tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-        icon: "group",
-    },
-];
 
 export const education = [
     {
@@ -286,7 +170,7 @@ export const footer = [
             },
             {
                 text: "Clone this page",
-                link: "https://github.com/vinaysomawat/itsonlytamim.github.io",
+                link: "https://github.com/itsonlytamim/itsonlytamim.github.io",
             },
         ],
     },
